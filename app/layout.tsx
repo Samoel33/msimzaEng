e1d986466ba484dfbaa642124ab33abd0e909dd4
Samoel33 <68919054+Samoel33@users.noreply.github.com>
2024-07-23 11:46:25 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/Components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mshimza Engineering",
+  title: "Msimza Engineering",
   description: "Mshimza Egnineering Services at level best",
 };
 
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`inter.className flex flex-col min-h-screen`}>
+      <body className={` flex flex-col min-h-screen`}>
         <Navigation/>
        <main className="overflow-x-hidden">
         {children}
